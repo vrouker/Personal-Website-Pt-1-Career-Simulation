3dddc1a304987f20966f59a033f027c6f20fa604
vrouker <vrouker@gmail.com>
2025-02-15 15:00:50 -0500

    updated JS with a console log message

--- javascript.js
+++ javascript.js
@@ -1,12 +1,1 @@
-function goTohome() {
-    window.location.href="home.html"}
-
-function goToabout() {
-    window.location.href="about.html"}
-
-function gotToportfolio() {
-    window.location.href="portfolio.html"}
-
-    console.log(goTohome)
-    console.log(goToabout)
-    console.log(gotToportfolio)
+console.log("Hello Ari!")
